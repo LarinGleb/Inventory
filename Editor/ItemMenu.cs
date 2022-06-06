57be140261784bd9c486f1879d8b4b5f50ea4d66
VforVindetta <88457706+VforVindetta@users.noreply.github.com>
2022-06-06 22:54:38 +0300

    Added a menu on toolbar in Unity Editor which clear item and delete all

---
+++ Editor/ItemMenu.cs
@@ -0,0 +1,34 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+public class VoidItem
+{
+
+    [MenuItem("Inventory/Add New Item", priority = -11)]
+    private static void CreateVoidItem() {
+        GameObject BasicItem = (GameObject)PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath<Object>("Assets/Inventory/Prefabs/BasicItem.prefab"));
+        BasicItem.name = "BasicItem";
+
+        BasicItem.transform.localPosition = Vector3.zero;
+        BasicItem.transform.localEulerAngles = Vector3.zero;
+        Selection.activeGameObject = BasicItem;
+    }
+
+    [MenuItem("Inventory/Clear Item", priority = -12)]
+    private static void ClearItem() {
+        Selection.activeGameObject.GetComponent<Item>().ClearItem();
+
+    }
+
+    [MenuItem("Inventory/Delete All Items", priority = -13)]
+    private static void DeleteAllItems() {
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag("Item"))
+        {
+            MonoBehaviour.DestroyImmediate(item);
+        }
+
+    }
+}
+
+#endif
